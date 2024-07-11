a143fea5d8b55517221779be4c6bcd6455061e08
Tairea <jamin.tairea@gmail.com>
2024-07-11 10:56:27 -0500

    v1 ready for for deployment

---
+++ README.md
@@ -0,0 +1,2 @@
+# EDT Submission form
+This is project includes the online form for new submissions to the EDT Notion database  
---
+++ server.js
@@ -0,0 +1,55 @@
+require("dotenv").config()
+const 
+  cors = require('cors'),
+  express = require("express"),
+  serveStatic = require("serve-static"),
+  history = require('connect-history-api-fallback')
+
+const app = express()
+
+const { Client } = require("@notionhq/client")
+const notion = new Client({ auth: process.env.NOTION_KEY })
+// const toolDatabase = process.env.NOTION_DATABASE_ID
+// const resourceDatabase = process.env.NOTION_DATABASE_ID
+// const hardwareDatabase = process.env.NOTION_DATABASE_ID
+
+// http://expressjs.com/en/starter/static-files.html
+app.use(serveStatic("public"))
+app.use(express.json()) // for parsing application/json
+app.use(cors())
+
+// Get database properties
+app.post("/database", async function (request, response) {
+  response.set('Access-Control-Allow-Origin','*')
+
+  try {
+    const database = await notion.databases.retrieve({ database_id: request.body.database });
+    response.json({ message: "success!", data: database })
+  }  
+  catch (error) {
+    response.json({ message: "error", error })
+  }
+})
+
+// Create new page. The database ID is provided in the web form.
+app.post("/pages", async function (request, response) {
+  response.set('Access-Control-Allow-Origin','*')
+  console.log('body: ', request.body)
+
+  try {
+    const newPage = await notion.pages.create({
+      parent: {
+        database_id: request.body.databaseId,
+      },
+      properties: request.body.properties
+    })
+    response.json({ message: "success!", data: newPage })
+  } catch (error) {
+    response.json({ message: "error", error })
+  }
+})
+
+// listen for requests :)
+const listener = app.listen((process.env.PORT || 3000), function () {
+  console.log("Your app is listening on port " + listener.address().port)
+})
